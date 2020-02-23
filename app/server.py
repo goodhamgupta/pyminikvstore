adb5f7dbdc364e37605e4b682e84a9b17de5957a
Shubham Gupta <shubhamg2208@live.com>
2020-02-23 18:27:43 +0800

    Add filecache class

--- app/server.py
+++ app/server.py
@@ -1,4 +1,6 @@
+import hashlib
 import os
+import socket
 import sys
 import time
 
@@ -11,32 +13,70 @@ if os.environ["TYPE"] == "master":
 
 def master(env, start_response):
     key = env.get("REQUEST_URI")[1:]
-    request_type = env.get('REQUEST_METHOD')
-    if request_type == "GET":
-        print(key)
-        metakey = db.get(key.encode('utf-8'))
-        print("HERE")
-        if metakey:
-            # key found
-            meta = json.loads(metakey)
-            # send the redirect
-            headers = [{'location': f"http://{meta.get('volume')}/{key}", 'expires': '100'}]
-            start_response("302 Found", headers)
-            return [meta]
-        else:
-            start_response("404 Not Found", [("Content-Type", "application/json")])
-            return ["Value not found for key: {}".format(key).encode()]
-    elif request_type == "PUT":
-        # Fetch data from the payload and use db.put
+    request_type = env.get("REQUEST_METHOD")
+    metakey = db.get(key.encode("utf-8"))
+
+    if metakey is None:
+        if request_type == "PUT":
+            # TODO handle putting key
+            pass
+        # this key doesn't exist and we aren't trying to create it
+        start_response("404 Not Found", [("Content-Type", "application/json")])
+        return ["Value not found for key: {}".format(key).encode()]
+
+    # key found
+    meta = json.loads(metakey)
+    # send the redirect
+    headers = [{"location": f"http://{meta.get('volume')}/{key}", "expires": "100"}]
+    start_response("302 Found", headers)
+    return [meta]
+
+
+class FileCache(object):
+    def __init__(self, basedir):
+        os.makedirs(basedir)
+        self.basedir = basedir
+
+    def keytopath(self, key):
+        # multilayer nginx
+        path = basedir + "/" + key[0:1] + "/" + key[1:2]
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        return os.path.join(path, key[2:])
+
+    def exists(self, key):
+        return os.path.isfile(self.keytopath(key))
+
+    def delete(self, key):
+        os.path.unlink(self.keytopath(key))
+
+    def get(self, key):
+        return open(self.keytopath(key), 'rb').read()
+
+    def put(self, key, value):
+        with open(self.keytopath(key), 'wb') as file:
+            file.write(value)
         pass
 
 
 if os.environ["TYPE"] == "volume":
-    import socket
-
     host = socket.gethostname()
+
+    # register with master
+    master = os.environ["MASTER"]
+
+    # create the filecache
+    FileCache(os.environ["VOLUME"])
 
 
 def volume(env, start_response):
-    start_response("200 OK", [("Content-Type", "text/html")])
-    return [b"Volume server mate"]
+    key = env["REQUEST_URI"].encode("utf-8")
+    hashed_key = hashlib.md5(key).hexdigest()
+    request_type = env.get("REQUEST_METHOD")
+
+    if request_type == "GET":
+        if not fc.exists(key):
+            # key not found in filecache
+            start_response("404 Not Found", [("Content-Type", "application/json")])
+            return ["Value not found for key: {}".format(key).encode()]
+    return FileCache.get(key)
